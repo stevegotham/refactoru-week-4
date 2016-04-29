24db2287276ccdd201c9cd9621b2fd60df31b8c4
Steve Gotham <g23design@gmail.com>
2016-04-29 16:11:14 -0600

    updating

--- inspirational-quotes/controller.js
+++ inspirational-quotes/controller.js
@@ -1,14 +1,16 @@
 angular.module('app')
-  .controller('controller',['myFactory', controllerFunction])
+  .controller('controller',['myFactory','$localStorage', controllerFunction])
 
-  function controllerFunction (myFactory) {
+  function controllerFunction (myFactory,$localStorage) {
     var cont = this
-
     cont.listOfQuotes = myFactory.listOfQuotes
+
     cont.quote
     cont.author
     cont.show = false
     cont.rating = cont.listOfQuotes.rating
+    cont.popupDiv = false
+    cont.currentQuote
 
     cont.Quote = function(quote,author) {
       this.quote = quote
@@ -20,10 +22,13 @@ angular.module('app')
       cont.listOfQuotes.unshift(cont.newestQuote)
       cont.newQuote = ''
       cont.newAuthor = ''
+      cont.storeData()
     }
 
-    cont.displayQuoteOptions = function() {
-      
+    cont.displayQuoteOptions = function(quote) {
+
+      cont.popupDiv = true
+      cont.currentQuote = quote
     }
 
     cont.authorList = function(quote) {
@@ -35,4 +40,21 @@ angular.module('app')
       cont.quoteAuthor = ''
       cont.show = false
     }
+
+    cont.clearIt = function() {
+      // console.log(cont.currentQuote)
+      cont.currentQuote.rating = cont.userRating
+      $localStorage.quotes = cont.listOfQuotes
+      cont.popupDiv = false
+      cont.userRating = ''
+    }
+
+    cont.deleteIt = function() {
+      console.log(cont.currentQuote)
+    }
+
+    cont.storeData = function() {
+      $localStorage.quotes = cont.listOfQuotes
+    }
+
   }
--- inspirational-quotes/factory.js
+++ inspirational-quotes/factory.js
@@ -1,23 +1,23 @@
 angular.module('app')
-  .factory('myFactory', [factoryFunction])
+  .factory('myFactory', ['$localStorage', factoryFunction])
 
-  function factoryFunction () {
+  function factoryFunction ($localStorage) {
 
     listOfQuotes = [
       {
         quote: 'This has been a wild ride',
         author: 'Steve',
-        rating: 6,
+        rating: 2,
       },
       {
         quote: 'Remember, it was much harder a week ago',
         author: 'Steve',
-        rating: 6,
+        rating: 4,
       },
       {
         quote: 'Get yourself a beer',
         author: 'Steve',
-        rating: 6,
+        rating: 5,
       },
       {
         quote:'If debugging is the process of removing software bugs, then programming must be the process of putting them in.',
@@ -27,21 +27,22 @@ angular.module('app')
       {
         quote:'The best method for accelerating a computer is the one that boosts it by 9.8 m/s2.',
         author:'Anonymous',
-        rating: 8,
+        rating: 5,
       },
       {
         quote:'Walking on water and developing software from a specification are easy if both are frozen.',
         author:'Edward V Berard',
-        rating: 0,
+        rating: 1,
       },
       {
         quote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
         author:'Brian Kernighan',
-        rating: 6,
+        rating: 4,
       },
     ]
 
     return {
-      listOfQuotes : listOfQuotes,
+      listOfQuotes : $localStorage.quotes ? $localStorage.quotes : listOfQuotes,
+
     }
   }
